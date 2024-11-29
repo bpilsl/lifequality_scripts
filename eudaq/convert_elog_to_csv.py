8848d566c645867e723001ab29ccca5a3dbc327b
Bernhard Pilsl <bernhard.pilsl@cern.ch>
2024-11-29 16:50:01 +0100

    adding all columns to csv file

--- eudaq/convert_elog_to_csv.py
+++ eudaq/convert_elog_to_csv.py
@@ -1,17 +1,12 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-
-import pandas as pd
 import numpy as np
 import re
 from bs4 import BeautifulSoup 
 import requests as req 
-import os
 import csv
 import argparse
-
-from mesonbuild.mesonlib import run_once
 
 #Parsing Path, Start and end ID for extraction
 parser = argparse.ArgumentParser()
@@ -30,14 +25,14 @@ base_path = args.base_path
 output_file = args.output_file
 
 #keys to extract from the elog
-keys_to_extract = ["Run Number", "DUT", "thr", "HV", "scan-val", "Subject", "Duration_min", "Biasing"]   
-keys_to_write = ["RunNumber", "DUT", "thr", "HV", "scan-val", "Subject", "Duration_min", "Biasing", "geometry", "outputDir"]
+# keys_to_extract = ["Run Number", "DUT", "thr", "HV", "scan-val", "Subject", "Duration_min", "Biasing"]   
+keys_to_write = []
 #ids to extract from elog
 ids = np.arange(args.border_start,args.border_end)
 
 
 #function to read in a single id and returns the values for the keys
-def extract_run_parameters(id_number):    
+def extract_run_parameters(id_number, first_call):    
     #modifying path for specific id and read in raw data
     output = []
     path = f'{base_path}'+f'/{id_number}' 
@@ -46,34 +41,52 @@ def extract_run_parameters(id_number):
     table_rows = data.find_all('input')
 
     #loop through raw data and extract "value=" for given keys
-    for key in keys_to_extract:
+    # for key in keys_to_extract:
+    try:
         for row in table_rows:
             row = str(row)
-            if re.search(f'input name="{key}"', row):
-                value = re.findall(r'value=\"(.+)\"', row)
-                if not value:
-                    output.append("nan")
+            match = re.search('input name="(.+?)"', row)
+            if match:
+                input_name = match.group(1)
+                if first_call:                
+                    keys_to_write.append(input_name)
                 else:
-                    output.append(value[0])
-    output[0] =  str(output[0].zfill(3))       
+                    value = re.findall(r'value=\"(.+)\"', row)      
+                    if input_name == 'Run Number':
+                        # print('before', value[0])
+                        value[0] = value[0].zfill(3)
+                        # print('after', value[0])
+
+                        if (not value or len(value) == 0):
+                            raise Exception(f'entry {id_number} without Run Number')
+
+                    if not value:
+                        output.append("nan")
+                    else:
+                        output.append(value[0])            
+        
+        output[0] =  str(output[0].zfill(3))
+    except Exception as ex:
+        print(ex)
+        output = None
     return output
 
-
-
-#check for existing output file and remove it
-if os.path.exists(output_file):
-    os.remove(output_file)
+extract_run_parameters(1, True) #do this to populate available keys, really gross but whatever
+keys_to_write.extend(["geometry", "outputDir"])
 
 #write csv file 
-with open(output_file, 'a') as table: 
+with open(output_file, 'w') as table: 
     writer = csv.writer(table, delimiter=',')
     writer.writerow(keys_to_write)
     for i in ids:
         #ensure that only entries with run number are inserted
-        run_params = extract_run_parameters(i)
-        if run_params[0]!="nan":
-            writer.writerow(run_params)
-        print('done with run', i)
+        try:
+            run_params = extract_run_parameters(i, False)
+            if run_params is not None and run_params[0]!="nan":
+                writer.writerow(run_params)
+            print('done with run', i)
+        except Exception as ex:
+            print(ex)
     table.close()
     
 
